69e400797dd0ad89739cd79a2164bc4a5880c4c9
gusdn0413 <gusdn0413@naver.com>
2024-07-26 16:08:41 +0900

    initial commit

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,34 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+@Transactional
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager entityManager;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		entityManager.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(entityManager);
+		QHello qHello = new QHello("h");
+
+		Hello result = query.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertEquals(hello, result);
+		Assertions.assertEquals(hello.getId(), result.getId());
+	}
+}
